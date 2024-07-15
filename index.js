a1d6b30dae3fa10b5ba7b30b26800b77ce268216
vijayvs <vijayvs6383@gmail.com>
2024-07-15 19:56:36 +0530

    route structure

--- index.js
+++ index.js
@@ -11,6 +11,28 @@ app.get("/", (req,res) => {
     res.json({msg: "hello students!"})
 })
 
+//CRUD functionality of movies
+
+// R - For reading
+app.get('/movies',() => {
+
+})
+
+// C - For creating movies
+app.post('/movies',() => {
+
+})
+
+// U - For updating movies
+app.put('/movies/:id',() => {
+
+})
+
+// D - For deleting movies
+app.delete('/movies:/id',() => {
+
+});
+
 app.listen(port, () => {
     console.log(`the server is running at http://localhost:${port}`)
 })
