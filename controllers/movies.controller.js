377525072377cbbb110d6a4ab9b9dfbe84a98ade
vijayvs <vijayvs6383@gmail.com>
2024-07-16 12:37:13 +0530

    update functionality

--- controllers/movies.controller.js
+++ controllers/movies.controller.js
@@ -1,10 +1,15 @@
 import Movie from "./models/movie.model.js";
 
-export const movieIndex = (req, res) => {
-    res.send("get all movie list")
+export const movieIndex = async (req, res) => {
+    try {
+        const movies = await Movie.find()
+        res.json(movies);
+    } catch (error) {
+        res.status(500).json({ message: error.message })
+    }
 };
 
-export const movieCreate = async(req, res) => {
+export const movieCreate = async (req, res) => {
     // id, title, desc
 
 
@@ -14,11 +19,11 @@ export const movieCreate = async(req, res) => {
         desc: req.body.desc
     })
 
-    try{
+    try {
         const movie = await newMovie.save();
         return res.status(201).json(movie);
-    }catch(error){
-        return res.status(400).json({message: error.message})
+    } catch (error) {
+        return res.status(400).json({ message: error.message })
     }
 
     return res.json(req.body);
@@ -26,8 +31,54 @@ export const movieCreate = async(req, res) => {
     //create the movie info
 }
 
-export const movieUpdate = (req, res) => {
-    res.send("update a movie")
+export const movieDetail = async (req, res) => {
+    try {
+        const movie = await Movie.findById(req.params.id);
+
+        if (movie == null) {
+            return res.status(404).json({ message: "Cannot find movie" });
+        }
+        else {
+            res.json(movie);
+        }
+    } catch (error) {
+        return res.status(500).json({ message: error.message });
+    }
+}
+
+export const movieUpdate = async (req, res) => {
+    try {
+        const updatedMovie = await Movie.findOneAndUpdate(
+            { _id: req.params.id },
+            {
+                title: req.body.title,
+                desc: req.body.desc
+            },
+            {
+                new: true,
+            }
+        );
+        res.status(200).json(updatedMovie);
+    }catch(error){
+        res.status(400).json({message: error.message});
+    }
+
+    // validate the user input
+
+    // if(req.body.title != null){
+    //     res.movie.title = req.body.title;
+    // }
+    // if(req.body.desc != req.body.title){
+    //     res.movie.desc = req.body.desc;
+    // }
+
+    // try{
+    //    const updatedMovie =  await res.movie.save();
+    //    res.json(updatedMovie);
+    // }catch(error){
+    //     res.status(400).json({message: error.message});
+    // }
+
 };
 
 export const movieDelete = (req, res) => {
--- routes/movies.routes.js
+++ routes/movies.routes.js
@@ -1,16 +1,20 @@
 import express from "express"
-import { movieCreate, movieDelete, movieIndex, movieUpdate } from "../controllers/movies.controller.js";
+import { movieCreate, movieDelete, movieDetail, movieIndex, movieUpdate } from "../controllers/movies.controller.js";
 
 const router = express.Router();
 
 // R - For reading
 router.get('/',movieIndex)
 
+router.get('/:id', movieDetail);
+
 // C - For creating movies
 router.post('/',movieCreate);
 
+
+
 // U - For updating movies
-router.put('/id',movieUpdate)
+router.put('/:id',movieUpdate)
 
 // D - For deleting movies
 router.delete('/id',movieDelete);
